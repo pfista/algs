4fedd3486a3942a0c47f6b81da53d0d2a727d07f
pfista_cs <pfista@utexas.edu>
2013-10-06 15:42:05 -0500

    Initial commit

---
+++ hw5/README.md
@@ -0,0 +1,8 @@
+### lca.py
+
+Implement a dynamic programming algorithm to compute the LCA matrix given a
+rooted binary tree with all nodes labelled.
+
+Input: A rooted binary tree, given as an adjacency list.  
+Output: LCA matrix such that LCA[x,y] is the least common ancestor for leaves x
+and y. 
---
+++ hw5/lca.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python2.7
+""" Create an LCA Matrix representing node ancestors """
+
+class Node:
+    "Represents a node in a binary tree"
+    def __init__(self, data):
+        self.left = None
+        self.right = None
+        self.parent = None
+        self.data = data # A list that contains all child nodes
+
+    def private(self):
+        """ This """
+        pass
+
+    def insert_node(self):
+        """ THIS """
+
+
+class lcaMatrix:
+    """ Compute the LCA matrix """
+    def __init__(self, root):
+        self.root = root
+
+    def parse_input(self):
+        pass
+
+    
+
+
+def main():
+    print "hi"
+
+
+
+main()
