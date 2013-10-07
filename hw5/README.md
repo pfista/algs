7cac05348dc88ba8cda52b202003cab295341316
pfista_cs <pfista@utexas.edu>
2013-10-07 18:36:48 -0500

    LCA tree is generated correctly

--- hw5/README.md
+++ hw5/README.md
@@ -1,5 +1,13 @@
-### lca.py
+## Least Common Ancestor
+Michael Pfister
+mp25234
 
+### Usage
+./lca.py input.txt
+
+LCA Matrix will be saved to output.txt
+
+### Problem
 Implement a dynamic programming algorithm to compute the LCA matrix given a
 rooted binary tree with all nodes labelled.
 
--- hw5/lca.py
+++ hw5/lca.py
@@ -11,16 +11,28 @@ class Node():
         self.right = right
         self.parent = parent
 
-def get_list(nodes, key):
+def get_list(lca, nodes, key):
+    """ Generate the LCA matrix and store it in lca """
     node = nodes[key]
+    lca[key-1][key-1] = key
     if node.left is None and node.right is None:
         return [key]
     elif node.left is None:
-        return [key] + get_list(nodes, node.right)
+        return [key] + get_list(lca, nodes, node.right)
     elif node.right is None:
-        return [key] + get_list(nodes, node.left)
+        return [key] + get_list(lca, nodes, node.left)
     else:
-        return [key] + get_list(nodes, node.left) + get_list(nodes, node.right)
+        left_children = get_list(lca, nodes, node.left)
+        right_children = get_list(lca, nodes, node.right)
+        for i in range(len(left_children)):
+            for j in range(len(right_children)):
+                lca[left_children[i]-1][right_children[j]-1] = key
+                lca[right_children[j]-1][left_children[i]-1] = key
+                lca[key-1][left_children[i]-1] = key
+                lca[key-1][right_children[j]-1] = key
+                lca[left_children[i]-1][key-1] = key
+                lca[right_children[j]-1][key-1] = key
+        return [key] + left_children + right_children
 
 def main():
     """ Parse file input, assemble a dictionary, assemble a 2d matrix which
@@ -29,6 +41,7 @@ def main():
     parser.add_argument('input_file', metavar='f', help='input file', nargs='+')
     args = parser.parse_args()
 
+    # Open the input file to read
     lines = open(args.input_file[0], "r")
     
     nodes = dict()
@@ -37,7 +50,12 @@ def main():
     for line in lines:
         info = line.split()
         children = info[1].split(',')
-        key = int(info[0])
+        try:
+            key = int(info[0])
+        except ValueError:
+            print "malformed input, should be of format 'n (v,w) (p)'"
+            break
+        # Handle improper input by setting as None
         try:
             left = int(children[0][1:])
         except ValueError:
@@ -60,11 +78,20 @@ def main():
     # Create a list where each index i represents node i+1 and all contained
     # child nodes
     lists = [[]*len(nodes) for x in xrange(len(nodes))]
+    # Store LCA matrix here
+    lca = [[0 for j in range(len(nodes))]for x in xrange(len(nodes))]
+
     for key in nodes:
-        lists[key-1] = (get_list(nodes, key))
+        lists[key-1] = (get_list(lca, nodes, key))
 
-    print lists
-
+    # Output resulting LCA matrix to file
+    output = open("output.txt", 'w')
+    for i in lca:
+        for j in i:
+            s = str(str(j)+' ')
+            output.write(s)
+        output.write('\n')
+    output.close()
 
 if __name__ == '__main__':
     main()
