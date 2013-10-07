66daa8839e4d05a98776825e6bdcc0084cfb8a45
pfista_cs <pfista@utexas.edu>
2013-10-07 16:19:00 -0500

    added list for each nodes children set

--- hw5/lca.py
+++ hw5/lca.py
@@ -1,15 +1,30 @@
 #!/usr/bin/python2.7
-
-import argparse
 """ Create an LCA Matrix representing node ancestors """
 
+import argparse
+
 class Node():
+    """ Represents a node in a binary tree, when can have a left and right child
+    as well as a parent """
     def __init__(self, left, right, parent):
         self.left = left
         self.right = right
         self.parent = parent
 
+def get_list(nodes, key):
+    node = nodes[key]
+    if node.left is None and node.right is None:
+        return [key]
+    elif node.left is None:
+        return [key] + get_list(nodes, node.right)
+    elif node.right is None:
+        return [key] + get_list(nodes, node.left)
+    else:
+        return [key] + get_list(nodes, node.left) + get_list(nodes, node.right)
+
 def main():
+    """ Parse file input, assemble a dictionary, assemble a 2d matrix which
+    provides O(1) lookup time for LCA of two keys in the dicitonary """
     parser = argparse.ArgumentParser()
     parser.add_argument('input_file', metavar='f', help='input file', nargs='+')
     args = parser.parse_args()
@@ -18,25 +33,37 @@ def main():
     
     nodes = dict()
 
+    # Create a dictionary of nodes for each line from the input file
     for line in lines:
         info = line.split()
         children = info[1].split(',')
-        if len(children) >= 2:
-            if children[0][1:]:
-                left = children[0][1:]
-            if children[1][:1]:
-                right = children[1][:1]
+        key = int(info[0])
+        try:
+            left = int(children[0][1:])
+        except ValueError:
+            left = None
+        except IndexError:
+            left = None
+        try:
+            right = int(children[1][:1])
+        except ValueError:
+            right = None
+        except IndexError:
+            right = None
         try:
             parent = int(info[2][1:-1])
         except ValueError:
             parent = None
 
-        nodes[int(info[0])] = Node(left, right, parent)
+        nodes[key] = Node(left, right, parent)
 
+    # Create a list where each index i represents node i+1 and all contained
+    # child nodes
+    lists = [[]*len(nodes) for x in xrange(len(nodes))]
     for key in nodes:
-        print key, nodes[key].left
-        print key, nodes[key].right
-        print key, nodes[key].parent
+        lists[key-1] = (get_list(nodes, key))
+
+    print lists
 
 
 if __name__ == '__main__':
