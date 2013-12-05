0862e1eef624f33f3c56f4adeb2bdb60ae8cb9a0
pfista_cs <pfista@utexas.edu>
2013-12-05 10:52:24 -0600

    Added witzel input graph

--- independent_set/independent_set.cpp
+++ independent_set/independent_set.cpp
@@ -10,7 +10,7 @@ int max_removable(vector<vector<int> > neighbors, vector<int> cover);
 vector<int> procedure_1(vector<vector<int> > neighbors, vector<int> cover);
 vector<int> procedure_2(vector<vector<int> > neighbors, vector<int> cover, int k);
 int cover_size(vector<int> cover);
-ifstream infile ("graph.txt");
+ifstream infile ("w_graph.txt");
 ofstream outfile ("sets.txt");
 
 int main()
