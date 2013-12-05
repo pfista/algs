c2e1362cfd52e1581fe9c2fc4fd5a3b526bdd767
Michael Pfister <pfista@gmail.com>
2013-12-05 13:48:01 -0600

    convert.py correctly converts to input for networkx

--- independent_set/convert.py
+++ independent_set/convert.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python2.7
 
 import argparse
-import re
 
 def main():
     """ Converts an adjacency matrix to an adjacency list """
@@ -11,20 +10,21 @@ def main():
 
     # Open the input file to read
     lines = open(args.input_file[0], "r")
-    
-    edges = []
-    vertices = []
+    output = open("output.txt", 'w')
 
-    vertex = 1
+    vertex = 1 # Start default vertex to 1 
     # Parse the data into an edge and vertex list
     for line in lines:
         data = line.split(' ')
 
+        output.write(str(vertex))
         for x in range(0, len(data)):
             if data[x] == '1':
-                print vertex, x
-
+                output.write(' '+str(x+1))
+        output.write('\n')
         vertex += 1
+
+    output.close()
             
 if __name__ == '__main__':
     main()
--- independent_set/networkx_independent_set.py
+++ independent_set/networkx_independent_set.py
@@ -2,9 +2,25 @@
 
 from networkx import *
 import matplotlib.pyplot as plt
+from networkx.algorithms.approximation import maximum_independent_set
+import argparse
 
-G = nx.read_adjlist("test.txt")
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('input_file', metavar='f', help='input file', nargs='+')
+    args = parser.parse_args()
 
-nx.draw(G)
-plt.show()
-plt.savefig("path.png")
+    G = nx.read_adjlist(args.input_file[0])
+
+    nx.draw(G)
+
+    plt.show()
+    plt.savefig("path.png")
+
+    S = maximum_independent_set(G)
+
+    print S
+
+
+if __name__ == '__main__':
+    main()
